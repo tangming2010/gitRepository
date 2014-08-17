0e6e83490bafa7de2de3d64f8ffb231407391358
Your Name <you@example.com>
2014-08-17 23:45:59 +0800

    add some python file

---
+++ 1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# print absolute value of an integer
+#a=raw_input("Input a number:")
+a=-100
+if a>=0:
+	print a
+else:
+	print -a
+
+
---
+++ hello.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+print('hello world')
+print("What's your name?")
+name=raw_input("How Can I Comment Python Source Code.")
+print "your name is",name
